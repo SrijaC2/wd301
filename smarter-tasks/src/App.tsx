fd8f20adb8d71da51e8d32b88fedbc41c5f0cb87
srija <srija@DESKTOP-BN7HD8R>
2023-09-18 18:01:02 +0530

    level 6

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,58 +1,119 @@
+// import {
+//   createBrowserRouter,
+//   Navigate,
+//   RouterProvider,
+// } from "react-router-dom";
+// import HomePage from "./pages/HomePage";
+// import TaskListPage from "./pages/TaskListPage";
+// import TaskDetailsPage from "./pages/TaskDetailsPage";
+// import Signin from "./pages/Signin";
+// import ProtectedRoute from "./ProtectedRoute";
+// import Layout from "./Layout";
+// import NotFound from "./pages/Notfound";
+// import React from 'react';
+// import ReactPlayground from './ReactPlayground';
+// import Form from './Form';
+
+
 import {
   createBrowserRouter,
-  Navigate,
   RouterProvider,
 } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import TaskListPage from "./pages/TaskListPage";
-import TaskDetailsPage from "./pages/TaskDetailsPage";
-import Signin from "./pages/Signin";
+import Notfound from "./pages/Notfound";
+import Signup from './pages/signup';
+import Signin from './pages/signin';
+import Dashboard from "./pages/dashboard";
 import ProtectedRoute from "./ProtectedRoute";
-import Layout from "./Layout";
-import NotFound from "./pages/Notfound";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Navigate to="/signin" replace />,
+//   },
+//   {
+//     path: "/signin",
+//     element: <Signin />,
+//   },
+//   {
+//     path: "/notfound",
+//     element: <NotFound />,
+//   },
+//   {
+//     element: (
+//       <ProtectedRoute>
+//         <Layout />
+//       </ProtectedRoute>
+//     ),
+//     children: [
+//       {
+//         path: "home",
+//         element: <HomePage />,
+//       },
+//       {
+//         path: "tasks",
+//         element: <TaskListPage />,
+//       },
+//       {
+//         path: "tasks/:id",
+//         element: <TaskDetailsPage />,
+//       },
+//       {
+//         path: "*",
+//         element: <Navigate to="/notfound" replace />,
+//       },
+//     ],
+//   }
+// ]);
+
+// const App = () => {
+
+//   return (
+    
+//     <>
+//     <Form />
+//       <ReactPlayground />
+      
+//       <RouterProvider router={router} />
+//     </>
+//   );
+// };
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to="/signin" replace />,
+    element: <Signup />,
   },
   {
-    path: "/signin",
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/signin", // then we've added the signin route
     element: <Signin />,
   },
   {
     path: "/notfound",
-    element: <NotFound />,
+    element: <Notfound />,
   },
   {
+    path: "/dashboard",
     element: (
       <ProtectedRoute>
-        <Layout />
+        <Dashboard />
       </ProtectedRoute>
     ),
-    children: [
-      {
-        path: "home",
-        element: <HomePage />,
-      },
-      {
-        path: "tasks",
-        element: <TaskListPage />,
-      },
-      {
-        path: "tasks/:id",
-        element: <TaskDetailsPage />,
-      },
-      {
-        path: "*",
-        element: <Navigate to="/notfound" replace />,
-      },
-    ],
+  },
+  {
+    path: "*",
+    element: <Notfound />,
   }
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
-};
+  return (
+    <RouterProvider router={router} />
+  );
+}
 
 export default App;
---
+++ smarter-tasks/src/pages/dashboard/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+
+const Dashboard: React.FC = () => {
+  const userDataString = localStorage.getItem('userData');
+  const navigate = useNavigate();
+  if (userDataString==='undefined' || userDataString===null) {
+    return (
+      <div className="min-h-screen  items-center justify-center bg-gray-100">
+        <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">
+         Dashboard
+        </h1>
+        <p> UserData Not Found</p>
+      </div>
+    );
+  }
+  const userData = JSON.parse(userDataString);
+  console.log(userData.name)
+  console.log(userData.email)
+  
+  const handleLogout = () => {
+    localStorage.removeItem('userData');
+    localStorage.removeItem('authToken');
+    navigate("/");
+  };
+  return (
+    <div className="min-h-screen  items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-center text-gray-800 pt-6">Dashboard</h1>
+      <button id='logout-link' onClick={handleLogout} className='m-6 text-white px-4 bg-gray-700 hover:bg-gray-800text-white font-semibold py-2  rounded-md focus:outline-none focus:shadow-outline-gray '>Logout</button>
+      <h2 id='name' className="text-xl  text-center text-gray-800 mb-8">{userData.name}</h2>
+      <p id='email ID'className="text-xl  text-center text-gray-800 mb-8">{userData.email}</p>
+      
+    </div>
+    
+  );
+}
+
+export default Dashboard;
