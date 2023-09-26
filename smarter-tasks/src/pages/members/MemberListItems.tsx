0c28ec81cb42de74fdf7b4dd16eda62b03662258
SrijaC2 <srijachanda06@gmail.com>
2023-09-26 23:47:49 +0530

    l7

--- smarter-tasks/src/pages/members/MemberListItems.tsx
+++ smarter-tasks/src/pages/members/MemberListItems.tsx
@@ -24,7 +24,7 @@ export default function MemberListItems() {
       {users.map((user: any) => (
         <div
           key={user.id} id="member"
-          className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          className="member block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <h5
             className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white"
